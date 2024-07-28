55f129574fe00825b57cff03560dc9a5bec4145b
Venkatesh Sridharan <vs498@njit.edu>
2024-07-27 20:57:54 -0400

    fixed port

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
-from flask import Flask, request, jsonify, render_template
-import joblib
-import pandas as pd
+import os
+from flask import Flask, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
-# Load the trained model
-model = joblib.load('fish_weight_predictor.joblib')
+# Load the model
+with open('model/fish_weight_predictor.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Fish Weight Predictor"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.form.to_dict()
-    df = pd.DataFrame([data])
-    df['Species'] = df['Species'].astype('category').cat.codes
-    prediction = model.predict(df)
+    data = request.json
+    prediction = model.predict([[
+        data['Length1'],
+        data['Length2'],
+        data['Length3'],
+        data['Height'],
+        data['Width']
+    ]])
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
