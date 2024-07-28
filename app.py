6bf88cb6f2f680038fbcaaec407b447185602652
Venkatesh Sridharan <vs498@njit.edu>
2024-07-27 21:26:26 -0400

    Added gunicorn

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pandas as pd
 import os
-from flask import Flask, request, jsonify
-import pickle
 
 app = Flask(__name__)
 
-# Load the model
-with open('model/fish_weight_predictor.pkl', 'rb') as f:
-    model = pickle.load(f)
+# Load the trained model
+model = joblib.load('fish_weight_predictor.joblib')
 
 @app.route('/')
 def home():
-    return "Fish Weight Predictor"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    prediction = model.predict([[
-        data['Length1'],
-        data['Length2'],
-        data['Length3'],
-        data['Height'],
-        data['Width']
-    ]])
+    data = request.form.to_dict()
+    df = pd.DataFrame([data])
+    df['Species'] = df['Species'].astype('category').cat.codes
+    prediction = model.predict(df)
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
+    # Get the port from the environment variable and set a default if not found
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # Run the app, listening on all interfaces on the specified port
+    app.run(host='0.0.0.0', port=port, debug=True)
